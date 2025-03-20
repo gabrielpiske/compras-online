c7b5357e067027c035064a664adab8f6dbfb6b53
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2025-03-19 21:18:28 -0300

    add: Project

---
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/AppLojaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.julioepiske.appLojaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLojaOnlineApplication.class, args);
+	}
+
+}
---
+++ appLojaOnline/src/test/java/com/julioepiske/appLojaOnline/AppLojaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioepiske.appLojaOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLojaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
