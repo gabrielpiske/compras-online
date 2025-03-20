a75a765ae7dd5a788a5eaf74aa4e145b760467cb
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-20 20:01:32 -0300

    add: userservice

--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/repository/UserRepository.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.julioepiske.appLojaOnline.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.julioepiske.appLojaOnline.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/UserService.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/UserService.java
@@ -1,5 +1,56 @@
 package com.julioepiske.appLojaOnline.service;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.julioepiske.appLojaOnline.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<User> lisAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findiById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    // método necessário do spring security
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário nçao encontrado com o email: " + email));
+
+        return User.builder()
+            .username(user.getName())
+            .password(user.getPassword())
+            .roles("USER")
+            .build();
+    }
 }
