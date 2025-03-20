0fa16a38ed0003e6791b3deff574e990f0fa189e
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-20 18:59:43 -0300

    add: classes and relationships

--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/model/Buy.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/model/Buy.java
@@ -1,5 +1,38 @@
 package com.julioepiske.appLojaOnline.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "buy")
 public class Buy {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    private Double priceTotal;
+
+    private LocalDateTime purchaseDate = LocalDateTime.now();
 }
--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/model/Store.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/model/Store.java
@@ -1,5 +1,27 @@
 package com.julioepiske.appLojaOnline.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Store {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String name;
+    private String description;
+    
+    @Lob
+    private byte[] logoImage;
 }
