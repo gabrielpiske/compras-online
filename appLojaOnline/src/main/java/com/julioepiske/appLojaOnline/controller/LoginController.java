1546702196d656e76499ff9c6f0ea661f1b394da
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-24 18:44:05 -0300

    fix: delete coments and imports

--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/LoginController.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.julioepiske.appLojaOnline.controller;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,7 +7,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.julioepiske.appLojaOnline.model.User;
 import com.julioepiske.appLojaOnline.service.UserService;
@@ -20,19 +18,16 @@ import lombok.AllArgsConstructor;
 public class LoginController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String showLoginPage(Model model) {
-        // Exibe qualquer mensagem de erro, se necessário
-        model.addAttribute("erro", ""); // Mensagem vazia inicialmente
+        model.addAttribute("erro", "");
         return "index";
     }
 
     @PostMapping("/register")
     public String registerUser(@Validated @ModelAttribute("user") User user,
             BindingResult result, Model model) {
-        // Verifica se houve erro de validação no formulário
         if (result.hasErrors()) {
             return "register"; // Se houver erros de validação, retorna para o formulário
         }
@@ -40,15 +35,13 @@ public class LoginController {
         // Verifica se o email já está cadastrado
         if (userService.findByEmail(user.getEmail()).isPresent()) {
             model.addAttribute("error", "Email já cadastrado.");
-            return "index"; // Retorna para a página de login com erro
+            return "index";
         }
 
-        // Codifica a senha antes de salvar
-        /* user.setPassword(passwordEncoder.encode(user.getPassword())); */
         userService.save(user);
 
         model.addAttribute("mensagem", "Cadastro realizado com sucesso!");
-        return "redirect:/login"; // Redireciona para o login após o registro
+        return "redirect:/login";
     }
 
     @GetMapping("/home")
