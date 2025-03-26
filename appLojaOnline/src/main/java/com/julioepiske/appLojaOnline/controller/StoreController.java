48a8333fa11f417e8b22672ec4b580b13e84d587
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-26 19:16:41 -0300

    add: crud store backend

--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/StoreController.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/StoreController.java
@@ -1,16 +1,50 @@
 package com.julioepiske.appLojaOnline.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.julioepiske.appLojaOnline.model.Store;
+import com.julioepiske.appLojaOnline.service.StoreService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @AllArgsConstructor
 @Controller
 public class StoreController {
 
+    private final StoreService storeService;
+
     @GetMapping("/store")
-    public String getStorePage() {
+    public String getStorePage(Model model) {
+        List<Store> stores = storeService.findAll();
+        model.addAttribute("stores", stores);
         return "store";
     }
+
+    @PostMapping("/store")
+    public String addStore(@ModelAttribute Store store) {
+        // TODO: process POST request
+        storeService.save(store);
+        return "redirect:/store";
+    }
+
+    @DeleteMapping("/store/{id}")
+    public String deleteStore(@PathVariable Long id) {
+        storeService.deleteById(id);
+        return "redirect:/store";
+    }
+
+    @PostMapping("/store/update")
+    public String updateStore(@ModelAttribute Store store) {
+        storeService.save(store);
+        return "redirect:/store";
+    }
+
 }
--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/StoreService.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/StoreService.java
@@ -1,5 +1,33 @@
 package com.julioepiske.appLojaOnline.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.julioepiske.appLojaOnline.model.Store;
+import com.julioepiske.appLojaOnline.repository.StoreRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class StoreService {
 
+    private final StoreRepository storeRepository;
+
+    public List<Store> findAll() {
+        return storeRepository.findAll();
+    }
+
+    public Store save(Store store) {
+        return storeRepository.save(store);
+    }
+
+    public Store findById(Long id) {
+        return storeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        storeRepository.deleteById(id);
+    }
 }
