7f3f283e7fa058763e3df1ab88036e20763701f3
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-27 19:12:56 -0300

    add: buy backend prototype

--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/BuyController.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/controller/BuyController.java
@@ -1,15 +1,41 @@
 package com.julioepiske.appLojaOnline.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.julioepiske.appLojaOnline.model.Store;
+import com.julioepiske.appLojaOnline.service.BuyService;
+import com.julioepiske.appLojaOnline.service.StoreService;
+
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @AllArgsConstructor
 @Controller
 public class BuyController {
+
+    private final BuyService buyService;
+    private final StoreService storeService;
+
     @GetMapping("/buy")
-    public String getBuyPage() {
+    public String getBuyPage(Model model) {
+        List<Store> stores = storeService.findAll();
+        model.addAttribute("stores", stores);
         return "buy";
     }
+
+    @PostMapping("/buy")
+    public String createPurchase(@RequestParam List<Long> storeIds) {
+        //TODO: process POST request
+        buyService.createPurchase(storeIds);
+        
+        return "redirect:/buy";
+    }
+    
 }
--- appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/BuyService.java
+++ appLojaOnline/src/main/java/com/julioepiske/appLojaOnline/service/BuyService.java
@@ -1,5 +1,46 @@
 package com.julioepiske.appLojaOnline.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.julioepiske.appLojaOnline.model.Buy;
+import com.julioepiske.appLojaOnline.model.Store;
+import com.julioepiske.appLojaOnline.model.User;
+import com.julioepiske.appLojaOnline.repository.BuyRepository;
+import com.julioepiske.appLojaOnline.repository.StoreRepository;
+import com.julioepiske.appLojaOnline.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class BuyService {
 
+    private final BuyRepository buyRepository;
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
+
+    public void createPurchase(List<Long> storeIds){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new RuntimeException("Usuário Não Encontrado"));
+
+        List<Store> stores = storeRepository.findAllById(storeIds);
+
+        //cria compras para as lojas selecionadas
+        for(Store store : stores){
+            Buy buy = new Buy();
+            buy.setUser(user);
+            buy.setStore(store);
+            buy.setPurchaseDate(LocalDateTime.now());
+            buyRepository.save(buy);
+        }
+    }
 }
